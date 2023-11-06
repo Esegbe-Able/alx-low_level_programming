edd275bd5d2f624279f4ff957931aaa3d0143191
Esegbe-Able <esegbeable6@gmail.com>
2023-11-06 01:31:39 -0800

    Preporcessor commit 1

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - The main file name prints current file name
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+		return (0);
+}
