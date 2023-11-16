64ce4da1c4208ff58da6b21edfd2bb6a345ca3d8
Esegbe-Able <esegbeable6@gmail.com>
2023-11-16 15:58:57 -0800

    updae

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -14,6 +14,7 @@ typedef struct list_s
 	struct list_s *next;
 }
 list_t;
+void first(void);
 int _putchar(char ch);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
