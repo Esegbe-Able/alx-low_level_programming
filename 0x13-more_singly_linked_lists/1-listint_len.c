6304ecea14cae0a6ef70d198ac0222071dde5a30
Esegbe-Able <esegbeable6@gmail.com>
2023-11-21 02:03:16 -0800

    Update changes

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 
 /**
- * listint_len - Is a function that returns the number of elements in a linked  list
- * @h: pointer 
+ * listint_len - Is a function that returns the
+ * number of elements in a linked  list
+ * @h: pointer
  * Return: number  of nodes
  */
 size_t listint_len(const listint_t *h)
